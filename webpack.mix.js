788c4609704cdac84ad4d5ec66a7f6aae529fdb5
Wilson Wong <wilsonwong2988@gmail.com>
2019-11-12 07:16:12 -0800

    Reconfigure Laravel Mix hot reload config for Apache

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,6 +12,14 @@ require('laravel-mix-bundle-analyzer');
  |
  */
 mix.setPublicPath('public/');
+
+mix.options({
+  hmrOptions: {
+    host: 'artbox.test',
+    port: 8080
+  }
+});
+
 mix.react('resources/js/app.js', 'public/js')
   .sass('resources/sass/app.scss', 'public/css');
 
