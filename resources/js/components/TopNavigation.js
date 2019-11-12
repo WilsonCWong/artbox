e874461a18f2e0da22b3cb98eaf18c02d1eefc89
Wilson Wong <wilsonwong2988@gmail.com>
2019-11-12 07:23:47 -0800

    Add dropdown to top navigation

--- resources/js/components/TopNavigation.js
+++ resources/js/components/TopNavigation.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useState, useEffect } from 'react';
+import styled, { css } from 'styled-components';
 import {
   Link,
   useLocation,
@@ -8,39 +8,113 @@ import {
 import axios from 'axios';
 import {
   Navbar,
-  Nav
+  Nav,
+  NavDropdown,
 } from 'react-bootstrap';
-import { useDispatch } from 'react-redux';
-import { Button } from '@material-ui/core';
+import { useDispatch, useSelector } from 'react-redux';
+import { Avatar, Button } from '@material-ui/core';
 import { authenticateUser } from "../actions/auth";
+import { updateUser } from "../actions/user";
+import { showNav } from "../actions/ui";
 
+const dropdownCss = css`
+  .dropdown-toggle::after {
+    display: none;
+  }
+  
+  a.dropdown-toggle.nav-link {
+    padding: 0;
+  }
+  
+  .dropdown-menu {
+    right: 0;
+    left: auto;
+  }
+  
+  @media only screen and (max-width: 576px) {
+    display: none;
+  }
+`;
+
+const UserAvatar = styled(Avatar)`
+  margin: 0;
+`;
+
+const UserContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  max-height: 40px;
+  padding: 0px 8px;
+`;
+
+const VerticalDivider = styled.span`
+  margin-left:12px;
+  margin-right:12px;
+  width:1px;
+  border-left:1px solid gray;
+  @media only screen and (max-width: 576px) {
+    display: none;
+  }
+`;
 
 function TopNavigation() {
+  const user = useSelector(state => state.user.user);
   const dispatch = useDispatch();
   const location = useLocation();
   const history = useHistory();
 
-  const LogOut = () => {
+  useEffect(() => {
+    return () => {
+      dispatch(showNav(false));
+    }
+  }, []);
+
+  const Logout = () => {
     axios.post('/logout').then(res => {
-      console.log(res);
+      dispatch(updateUser(null));
       dispatch(authenticateUser(false));
       history.push('/login');
     });
   };
 
   if (location.pathname === '/login' || location.pathname === '/register') {
+    dispatch(showNav(false))
     return null;
+  } else {
+    dispatch(showNav(true));
+  }
+
+  const dropdownDisplay = () => {
+      return (
+      <UserContainer>
+        <UserAvatar src={ (user?.profile_picture) ? `/storage${user?.profile_picture}` : '/images/user_placeholder.png' } />
+      </UserContainer>
+    )
+  };
+
+  const renderDropdown = () => {
+    return (
+      <NavDropdown css={dropdownCss} title={ dropdownDisplay() } id="nav-dropdown">
+        <NavDropdown.Item as={Link} to={`/profile/${user?.username || ''}`}>Profile</NavDropdown.Item>
+        <NavDropdown.Item as={Link} to="/settings">Settings</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item onClick={Logout}>Logout</NavDropdown.Item>
+      </NavDropdown>
+    )
   }
 
   return (
       <div>
         <Navbar collapseOnSelect expand='sm' fixed='top' bg="light" variant="light">
-          <Navbar.Brand href="#home">ArtBox</Navbar.Brand>
+          <Navbar.Brand as={Link} to='/'>ArtBox</Navbar.Brand>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="ml-auto">
-              <Nav.Link as={Link} to="/">Home</Nav.Link>
-              <Button color='secondary' variant='contained' onClick={LogOut}>Log Out</Button>
+              { renderDropdown() }
+              <VerticalDivider/>
+              <Nav.Link as={Link} to="/posts/create">Create Post</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
