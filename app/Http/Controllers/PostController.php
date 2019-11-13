06478e9cee6130b964e33efa5e2d014fc8ff9f37
Wilson Wong <wilsonwong2988@gmail.com>
2019-11-13 05:15:04 -0800

    Remove index from PostController

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -19,16 +19,6 @@ class PostController extends Controller
     }
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
