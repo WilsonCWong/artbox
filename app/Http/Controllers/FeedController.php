9ac648203a335e8a4dbbcf498a11e05a5654603c
Wilson Wong <wilsonwong2988@gmail.com>
2019-11-13 07:25:29 -0800

    Add user feed api

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -3,12 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\Post;
+use App\User;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
 
-    // The "Home" feed. Shows posts from all users of the site.
+    // The "Home" feed. Shows $quantity + $offset of posts from all users of the site.
     public function home($quantity) {
         $offset = request()->query('offset');
 
@@ -20,6 +25,22 @@ class FeedController extends Controller
                     ->limit($quantity)
                     ->get();
 
+        phpinfo();
+
         return response()->json(['posts' => $posts]);
+    }
+
+    // The user feed. Gets $quantity + offset of users from the database.
+    public function users($quantity) {
+        $offset = request()->query('offset');
+
+        $users = User::select(['id', 'username'])
+                    ->when($offset, function($query, $offset) {
+                        return $query->offset($offset);
+                    })
+                    ->limit($quantity)
+                    ->get();
+
+        return response()->json(['users' => $users]);
     }
 }
